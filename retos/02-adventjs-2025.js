084858ded813b3e3eb72a418ec0b79089f6375cb
crisdux <crisfer.4217@gmail.com>
2025-12-03 23:25:21 -0400

    add reto 2

---
+++ retos/02-adventjs-2025.js
@@ -0,0 +1,30 @@
+function manufactureGifts(giftsToProduce) {
+    return giftsToProduce.map((regalo) =>{
+        return (regalo.quantity >= 0) ? Array(regalo.quantity).fill(regalo.toy) : []
+    }).flat()
+}
+
+const production1 = [
+    { toy: 'car', quantity: 3 },
+    { toy: 'doll', quantity: 1 },
+    { toy: 'ball', quantity: 2 }
+]
+
+const result1 = manufactureGifts(production1)
+console.log(result1)
+// ['car', 'car', 'car', 'doll', 'ball', 'ball']
+
+const production2 = [
+    { toy: 'train', quantity: 0 }, // no se fabrica
+    { toy: 'bear', quantity: -2 }, // tampoco
+    { toy: 'puzzle', quantity: 1 }
+]
+
+const result2 = manufactureGifts(production2)
+console.log(result2)
+// ['puzzle']
+
+const production3 = []
+const result3 = manufactureGifts(production3)
+console.log(result3)
+// []
